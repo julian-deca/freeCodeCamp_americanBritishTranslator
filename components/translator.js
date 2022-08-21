4e083228c7c00d1e3c76835f99bb0963dee9e7ab
julian <juliandecastelli@gmail.com>
2022-08-21 16:43:35 -0300

    add translator logic and routing

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,73 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  useTranslation(translation, arr, aTB) {
+    arr.forEach((word, i) => {
+      for (const [key, value] of Object.entries(translation)) {
+        if (aTB) {
+          if (word.toLowerCase() == key) {
+            if (translation == americanToBritishTitles) {
+              arr[i] =
+                '<span class="highlight">' +
+                value.charAt(0).toUpperCase() +
+                value.slice(1) +
+                "</span>";
+            } else {
+              arr[i] = '<span class="highlight">' + value + "</span>";
+            }
+          }
+          if (/\d+\:\d+/g.test(word)) {
+            let first = word.slice(0, word.indexOf(":"));
+            let last = word.slice(word.indexOf(":") + 1);
+            arr[i] =
+              '<span class="highlight">' + first + "." + last + "</span>";
+          }
+        } else {
+          if (word.toLowerCase() == value) {
+            if (translation == americanToBritishTitles) {
+              arr[i] =
+                '<span class="highlight">' +
+                key.charAt(0).toUpperCase() +
+                key.slice(1) +
+                "</span>";
+            } else {
+              arr[i] = '<span class="highlight">' + key + "</span>";
+            }
+          }
+          if (/\d+\.\d+/g.test(word)) {
+            let first = word.slice(0, word.indexOf("."));
+            let last = word.slice(word.indexOf(".") + 1);
+            arr[i] =
+              '<span class="highlight">' + first + ":" + last + "</span>";
+          }
+        }
+      }
+    });
+    return arr;
+  }
+  translate(text, locale) {
+    let arr = text.split(" ");
+    let copy = text.split(" ");
+    let aTB;
+    if (locale == "american-to-british") {
+      aTB = true;
+    } else if (locale == "british-to-american") {
+      aTB = false;
+    }
+    arr = this.useTranslation(americanOnly, arr, aTB);
+    arr = this.useTranslation(americanToBritishSpelling, arr, aTB);
+    arr = this.useTranslation(americanToBritishTitles, arr, aTB);
+    arr = this.useTranslation(britishOnly, arr, aTB);
 
+    if (arr.join() == copy.join()) {
+      return "Everything looks good to me!";
+    }
+
+    return arr.join(" ");
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,28 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const text = req.body.text;
+    const locale = req.body.locale;
+
+    if (!locale || text == undefined) {
+      res.json({ error: "Required field(s) missing" });
+      return;
+    } else if (text == "") {
+      res.json({ error: "No text to translate" });
+      return;
+    } else if (
+      locale != "american-to-british" &&
+      locale != "british-to-american"
+    ) {
+      res.json({ error: "Invalid value for locale field" });
+      return;
+    }
+
+    res.json({ text, translation: translator.translate(text, locale) });
+  });
 };
